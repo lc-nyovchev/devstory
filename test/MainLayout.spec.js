12251b27bb3ca25678d0c688f56fb80b2005c828
nyo <1153009+lc-nyovchev@users.noreply.github.com>
2022-02-16 10:58:02 +0000

    Small cleanup

--- test/MainLayout.spec.js
+++ test/MainLayout.spec.js
@@ -1,10 +1,10 @@
 import { mount } from '@vue/test-utils'
 import { describe, assert, expect, test } from 'vitest'
-import HelloWorld from '../src/components/HelloWorld.vue'
+import MainLayout from '../src/components/MainLayout.vue'
 
 describe('', () => {
     test('asd', () => {
-        const wrapper = mount(HelloWorld)
+        const wrapper = mount(MainLayout)
         console.log('pass')
     })
 })
