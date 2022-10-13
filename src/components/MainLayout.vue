7d47628d7f1eaa772b7f34d476ea259b9eb73ac8
nyo <1153009+lc-nyovchev@users.noreply.github.com>
2022-10-13 10:43:48 +0000

    Changed size a bit

--- src/components/MainLayout.vue
+++ src/components/MainLayout.vue
@@ -1,6 +1,7 @@
 <template>
 	<v-card>
-		<v-img src="https://tse2.mm.bing.net/th?id=OIP.OHQD4CDglISK-pLzB_rb8wHaEK&pid=Api" />
+		Hola Amigos
+		<v-img src="https://tse2.mm.bing.net/th?id=OIP.OHQD4CDglISK-pLzB_rb8wHaEK&pid=Api" width="640" height="480" />
 	</v-card>
 </template>
 <script setup></script>
